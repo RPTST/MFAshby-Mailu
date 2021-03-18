5a674314ccc015eb7bf5ca3908c3e869240ad73f
Edward B <32598399+RPTST@users.noreply.github.com>
2021-03-18 06:28:45 -0400

    Add files via upload

---
+++ services/rspamd/start.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import jinja2
+import os
+import socket
+import glob
+
+convert = lambda src, dst: open(dst, "w").write(jinja2.Template(open(src).read()).render(**os.environ))
+
+# Actual startup script
+os.environ["FRONT_ADDRESS"] = socket.gethostbyname(os.environ.get("FRONT_ADDRESS", "front"))
+if "HOST_REDIS" not in os.environ: os.environ["HOST_REDIS"] = "redis"
+
+for rspamd_file in glob.glob("/conf/*"):
+    convert(rspamd_file, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
+
+# Run rspamd
+os.execv("/usr/sbin/rspamd", ["rspamd", "-i", "-f"])
